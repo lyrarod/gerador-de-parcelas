c7e3803f834c432d84c483afb380f431995def33
lyrarod <lyrarod@gmail.com>
2023-03-15 22:16:22 -0300

    ...

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Parcel } from "@/components/Parcel";
+import { Parcel } from "@/components/parcel";
 import styles from "@/styles/Home.module.css";
 
 export default function Page() {
